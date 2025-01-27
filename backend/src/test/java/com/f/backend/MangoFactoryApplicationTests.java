0337df290d8f0dbeb38c86386ca2d0766a8b92d1
fmahadyBD <fmahady01@gmail.com>
2025-01-27 15:38:33 +0600

    Initial Backend Commit

---
+++ backend/src/test/java/com/f/backend/MangoFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangoFactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
