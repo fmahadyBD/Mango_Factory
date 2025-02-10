f328cb89c2301834e54a085179df5c50c0baf072
fmahadyBD <fmahady01@gmail.com>
2025-02-10 21:45:26 +0600

    Organized the Code

--- backend/src/main/java/com/f/backend/controller/AdminToUserController.java
+++ backend/src/main/java/com/f/backend/controller/AdminToUserController.java
@@ -1,7 +1,6 @@
 package com.f.backend.controller;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +14,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.f.backend.entity.User;
